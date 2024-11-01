1b9b3b4bf7bd9758b657e71910325f8bbaf72af0
C3dex <95946744+C3dex@users.noreply.github.com>
2024-11-01 17:02:06 +0300

    Created bmi.py

---
+++ bmi.py
@@ -0,0 +1,11 @@
+weight = float(input("Enter Your Weight: ").strip());
+height = float(input("Enter Your Height: ").strip());
+x = weight/float(height * height)
+if x < 18.5:
+  print('Underweight')
+elif x>=18.5 and x < 25:
+  print('Normal')
+elif x>=25 and x<30:
+  print('Overweight')
+elif x>=30:
+  print('Obese')
