57f0ac60a4213fa077dbc48761ee691cb58de824
C3dex <95946744+C3dex@users.noreply.github.com>
2024-11-02 12:38:00 +0300

    Create magic8.py

---
+++ magic8.py
@@ -0,0 +1,22 @@
+# magic8.py
+import random
+input('Question: ');
+n = random.randint(1,9);
+if n == 1:
+  print('Yes - definitely.')
+elif n == 2:
+  print('It is decidedly so.')
+elif n == 3:
+  print('Without a doubt.')
+elif n == 4:
+  print('Reply hazy, try again.')
+elif n == 5:
+  print('Ask again later.')
+elif n == 6:
+  print('Better not tell you now.')
+elif n == 7:
+  print('My sources say no.')
+elif n == 8:
+  print('Outlook not so good.')
+elif n == 9:
+  print('Very doubtful.')
