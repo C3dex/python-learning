a58ea6c4997a589a495485db1954aae5eba2093f
C3dex <95946744+C3dex@users.noreply.github.com>
2024-11-03 00:23:32 +0300

    Create 99_Bottles_of_Beer.py

---
+++ 99_Bottles_of_Beer.py
@@ -0,0 +1,7 @@
+#"99 Bottles of Beer" is an old song that annoying kids, oops I mean everyone, sang on road trips to pass the time.
+#99_Bottles_of_Beer.py🍾
+for i in range(99, 0, -1):
+  print(f'{i} bottles of beer on the wall')
+  print(f'{i} bottles of beer')
+  print('Take one down, pass it around')
+
