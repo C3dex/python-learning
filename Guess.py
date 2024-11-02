9eb470763d1efef27cf4de7b246b9946f3c6d75b
C3dex <95946744+C3dex@users.noreply.github.com>
2024-11-02 23:46:32 +0300

    Update Guess.py

--- Guess.py
+++ Guess.py
@@ -1,6 +1,12 @@
-#Guess.py
+# Write code below 💖
 guess = 0
+tries = 0
 int(input('Guess the number: '))
 while guess !=6:
   guess = int(input("Guess the number again! "))
-print("You got it!")
+  tries += 1
+  if tries == 5:
+    print('You have reached the maximum amount of tries...')
+    break
+if guess == 6:
+  print("You got it!")
