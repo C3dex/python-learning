299e987101363a7310022157ae74a9b4795c172d
C3dex <95946744+C3dex@users.noreply.github.com>
2024-11-03 14:26:24 +0300

    Create Snapple Tea.py

---
+++ Snapple Tea.py
@@ -0,0 +1,17 @@
+#Snapple Tea.py
+# Snapple is a famous tea drink brand from Queens, New York. 
+# Each bottle cap comes with a silly fun fact.
+import random 
+A = random.randint(0,5)
+if A == 0:
+  print('Flamingos turn pink from eating shrimp.')
+elif A == 1:
+  print('The only food that doesn\'t spoil is honey.')
+elif A == 2:
+  print('Shrimp can only swim backwards.')
+elif A == 3:
+  print('A taste bud\'s life span is about 10 days.')
+elif A == 4:
+  print('It is impossible to sneeze while sleeping.')
+elif A == 5:
+  print('It is illegal to sing off-key in North Carolina.')
